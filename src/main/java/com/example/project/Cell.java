0ae52ba0b8a2a9d27ab08458f33d5f5e90013bbc
Steevus-anon <sm650106@wne.edu>
2026-04-29 19:16:40 -0400

    code: give Cell variable life/death state

--- src/main/java/com/example/project/Cell.java
+++ src/main/java/com/example/project/Cell.java
@@ -1,5 +1,21 @@
 package com.example.project;
 
 public class Cell extends Neighbor {
-    
+    private boolean alive;
+
+    public boolean isLiving() {
+        return alive;
+    }
+
+    public void birth() {
+        alive = true;
+    }
+
+    public void kill() {
+        alive = false;
+    }
+
+    public void toggle() {
+        alive = !alive;
+    }
 }

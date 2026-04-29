38ed41c46ca843f55d898d61bdf1fd8bfd2b23f5
Steevus-anon <sm650106@wne.edu>
2026-04-28 22:16:54 -0400

    test: initializeWindowTest

---
+++ src/test/java/com/example/project/GameOfLifeTests.java
@@ -0,0 +1,11 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class GameOfLifeTests {
+    @Test
+    void initializeWindowTest() {
+        new GameOfLife();
+    }
+}

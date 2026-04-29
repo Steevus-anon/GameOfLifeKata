10d6d9d010f13d90e19c3c8e1051164af65525d7
Steevus-anon <sm650106@wne.edu>
2026-04-29 18:51:42 -0400

    test: neighborAndCellExists

--- src/test/java/com/example/project/GameOfLifeTests.java
+++ src/test/java/com/example/project/GameOfLifeTests.java
@@ -1,11 +1,17 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class GameOfLifeTests {
     @Test
     void initializeWindowTest() {
-        new GameOfLife();
+        GameOfLife g = new GameOfLife();
+    }
+
+    @Test
+    void neighborAndCellExists() {
+        Neighbor n = new Neighbor();
+        Cell c = new Cell();
     }
 }

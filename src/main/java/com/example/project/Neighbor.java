c9a9e56437a85ed2cbb32120fceeca1b27503a66
Steevus-anon <sm650106@wne.edu>
2026-04-29 19:06:15 -0400

    code: add linking of neighbors

--- src/main/java/com/example/project/Neighbor.java
+++ src/main/java/com/example/project/Neighbor.java
@@ -1,5 +1,11 @@
 package com.example.project;
 
 public class Neighbor {
-    
+    private Neighbor neighborhood[] = new Neighbor[4];
+    private int neighborCount = 0;
+
+    public void addNeighbor(Neighbor n) {
+        neighborhood[neighborCount] = n;
+        neighborCount++;
+    }
 }

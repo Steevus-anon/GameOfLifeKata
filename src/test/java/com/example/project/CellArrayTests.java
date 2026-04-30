6f4951db17f264cac153581e7cbd0f5458056ad2
Steevus-anon <sm650106@wne.edu>
2026-04-30 11:40:00 -0400

    test: squareTest

--- src/test/java/com/example/project/CellArrayTests.java
+++ src/test/java/com/example/project/CellArrayTests.java
@@ -6,16 +6,29 @@ import org.junit.jupiter.api.*;
 public class CellArrayTests {
     @Test
     void initializeCellArrayTest() {
-        CellArray myCellArray = new CellArray(5, 5);
-        Neighbor internalArray[][] = myCellArray.getInternalArray();
+        CellArray cells = new CellArray(5, 5);
+        Neighbor internalArray[][] = cells.getInternalArray();
         assertEquals(5, internalArray.length);
     }
 
     @Test
     void toggleCellInArray() {
-        CellArray myCellArray = new CellArray(3, 3);
-        myCellArray.toggleCell(0, 0);
-        String s = myCellArray.toString();
+        CellArray cells = new CellArray(3, 3);
+        cells.toggleCell(0, 0);
+        String s = cells.toString();
         assertEquals("[1, 0, 0]\n[0, 0, 0]\n[0, 0, 0]\n", s);
+    }
+
+    @Test
+    void squareTest() {
+        CellArray cells = new CellArray(2, 2);
+        cells.toggleMultiple(
+            0, 0,
+            0, 1,
+            1, 0,
+            1, 1
+        );
+        cells.iterate();
+        assertEquals("[1, 1]\n[1, 1]\n", cells.toString());
     }
 }
